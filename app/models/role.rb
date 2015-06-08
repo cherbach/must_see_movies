ce095e1d6f1b666f800b7629b3b57d5b25ad50a3
cherbach <christinaherbach@dhcp-10-105-61-86.wireless.northwestern.private>
2015-06-08 15:30:01 -0500

    Must See Movies Complete

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,15 @@
 class Role < ActiveRecord::Base
+
+	validates :character_name, :presence => true
+
+	belongs_to :actor, :class_name => "Actor", :foeign_key => "actor_id"
+
+	def actor
+		return Actor.find_by({ :id => self.actor_id})
+	end
+
+	def movie
+		return Movie.find_by({ :id=>self.movie_id})
+	end
+
 end
